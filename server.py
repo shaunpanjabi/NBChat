5f137b943b07d9596f9f3c88076e0fe9e79b993b
genelo <g_lo_27@hotmail.com>
2016-09-04 00:32:31 -0700

    server code
    very ugly code by gene/ salvaged by shaun

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/data", methods = ("GET",))
+def handle_data():
+    convostr =''
+    for i in chat:
+        convostr+= i+ "\n"
+    return convostr
+
+@app.route("/meow", methods = ("POST",))
+def handle_meow():
+    spoken = request.values
+    chat.append(spoken['name'] + " : " + spoken['message'])
+    return "dicksoutfornonbiasedchat"
+
+if __name__=='__main__':
+    chat = ['dont be biased, bitch']
+    app.run(host='0.0.0.0')
